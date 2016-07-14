31b74af8a47eab800a5b3864caca2395ce7b3c5b
Jennifer <jennifersoden@hotmail.com>
2016-07-14 11:07:45 -0600

    add links to followers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '6fa367e00d00024c1387', '6ab23b5bd1697c5bcdc0622993d66a092f527302'
+  provider :github, ENV['github_api_key'], ENV['github_secret_key']
 end
